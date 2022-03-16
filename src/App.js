b3ea55bdacced1da0c713f05ed2ff0cbc75f25bd
Jussi Kujala <jussijustus@gmail.com>
2022-03-16 21:28:06 +0200

    json from database working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/ostoslistaBackend/'
 
 function App() {
+
+  const [tasks,setTasks] = useState([]);
+
+useEffect(() => {
+  axios.get(URL)
+  .then((response) => {
+    console.log(response.data);
+  }).catch(error =>{
+  });
+}, [])
+
   return (
     <div className="App">
-
+test
     </div>
   );
 }
